432e5923b4d9ffacb56167a7c52d042318161a68
sujat <sujatha_kvr@yahoo.com>
2021-04-21 17:57:30 -0400

    changes

--- src/main/java/com/ontarioinc/mediaapp/MediaAppApplication.java
+++ src/main/java/com/ontarioinc/mediaapp/MediaAppApplication.java
@@ -2,12 +2,8 @@ package com.ontarioinc.mediaapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import com.ontarioinc.mediaapp.db.UserRepository;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class MediaAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ontarioinc/mediaapp/rest/SecurityConfiguration.java
+++ src/main/java/com/ontarioinc/mediaapp/rest/SecurityConfiguration.java
@@ -1,12 +1,12 @@
 package com.ontarioinc.mediaapp.rest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -14,28 +14,24 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
-	@Autowired
-	private UserDetailsService userDetailsService;
-	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService);
+		auth.ldapAuthentication()
+				.userDnPatterns("uid={0},ou=people")
+				.groupSearchBase("ou=groups")
+				.contextSource()
+				.url("ldap://localhost:8389/dc=springframework,dc=org")
+				.and()
+				.passwordCompare()
+				.passwordEncoder(new BCryptPasswordEncoder())
+				.passwordAttribute("userPassword");
 	}
 	
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.httpBasic().
-		             and().csrf().disable()
-		            .authorizeRequests()
-		            .antMatchers("/").permitAll()   
-					.antMatchers("/playlists/**").hasRole("ADMIN")
-					.antMatchers("/songs/**").hasAnyRole("USER", "ADMIN")
+		httpSecurity.authorizeRequests()
+		            .anyRequest().fullyAuthenticated()
 					.and()
 					.formLogin();
-	}
-	
-	@Bean
-	public PasswordEncoder getPasswordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
 	}
 }
